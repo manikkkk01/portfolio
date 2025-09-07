85b74f4b2d38af7507bc956d10cc29b1fb11c231
manikkkk01 <manikkaushik099@gmail.com>
2025-09-07 20:01:01 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,28 +6,27 @@ const Hero = () => {
   return (
     <div id="home" className="hero">
       {/* Animated Spotlight Background */}
+      <div className="hero-spotlight-animated" />
       <div className="hero-spotlight one"></div>
-      <div className="hero-spotlight two"></div>
 
-      <img src={profile_img} alt="Profile" className="fade-in" />
-      <h1 className="fade-in">
-        <span>I'm Manik Kaushik</span> Web developer based in Noida
+
+      <img src={profile_img} alt="Profile" />
+      <h1>
+        <span>I'm Manik Kaushik</span>, Web developer based in Noida
       </h1>
-      <p className="fade-in">
+      <p>
         I’m a passionate Software Developer with hands-on experience in Java,
         HTML, CSS, JavaScript, and React.js. I specialize in building responsive,
         user-friendly web interfaces and love turning ideas into real-world
         applications.
       </p>
-      <div className="hero-action fade-in">
-        <div className="hero-connect">
-          <a href="http://www.linkedin.com/in/manikkkk01">Linked In</a>
-        </div>
+      <div className="hero-action">
+        <div className="hero-connect" >
+          <a href="http://www.linkedin.com/in/manikkkk01">Linked In</a></div>
         <div className="hero-resume">
           <a href="/src/assets/resume.pdf" target="_blank" rel="noopener noreferrer">
             My resume
-          </a>
-        </div>
+          </a></div>
       </div>
     </div>
   );
